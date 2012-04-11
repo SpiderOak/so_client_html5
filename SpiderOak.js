30baec3c7f52e20669285b9cb17b5c2a933f01ac
Ken Manheimer <ken.manheimer@gmail.com>
2012-04-11 19:11:35 -0400

    Adjust the login address to something that is useful for origins outside of the SpiderOak.com domain, and set it to a private var in the new spideroak singleton instance.
    Refactor the spideroak object definition so it's a singleton instance
    framed with Crockford's module singleton pattern.
    
    This all seems proper and operational, *except* that it's currently blocked
    from successful XmlHTTPRequests when situated outside of SpiderOak.com due
    to cross-domain XHR request restrictions, evidenced by the browser warning
    (showing in the javascript debugging console in Chrome):
    
       XMLHttpRequest cannot load https://spideroak.com/browse/login.
       Origin http://myriadicity.net is not allowed by Access-Control-Allow-Origin.
    
    This stackoverflow answer describes the problem pretty closely:
    
       http://stackoverflow.com/questions/3595515/xmlhttprequest-error-origin-null-is-not-allowed-by-access-control-allow-origin#answer-3744697
    
    I could confirm the problem (and determine whether the code I'm using would
    work, in the absence of the problem) if I could run the script from a page
    visited within the SpiderOak.com domain.
    
    In any case, I'm going to need to get it working from a local file, and
    using a jsonP XmlHTTPRequest seems to be the best prospect for doing that.

--- SpiderOak.js
+++ SpiderOak.js
@@ -8,42 +8,53 @@
 
 
 $(document).ready(function() {
+    spideroak.init();
     $('.nav_login_storage form').submit(function () {
         var username = $('input[name=username]', this).val();
         var password = $('input[name=password]', this).val();
-        SpiderOak.remote_login({username: username, password: password});
+        spideroak.remote_login({username: username, password: password});
         return false;
     });
 });
 
-var SpiderOak = new function() {
-    this.remote_login = function (login_info, url) {
-        $.ajax({
-            url: url || '/browse/login',
-            type: 'POST',
-            dataType: 'text',
-            data: login_info,
-            success: function (data) {
-                var match = data.match(/^(login|location):(.+)$/m);
-                if (!match) {
-                    alert(translate('Temporary server failure.'
-                                    + ' Please try again in a few minutes.'));
-                } else if (match[1] == 'login') {
-                    remote_login(login_info, match[2]);
-                } else {
-                    window.location.href = match[2];
-                }
+/* Modular singleton pattern: */
+var spideroak = function() {
+    /* private: */
+    var storage_root = 'https://spideroak.com/browse/login';
+
+    /* public: */
+    return {
+        init: function () {
+            /* Nothing, yet. */
             },
-            error: function (xhr) {
-                if (xhr.status == 403) {
-                    alert(translate('Incorrect username or password.'));
-                } else if (xhr.status == 404) {
-                    alert(translate('Incorrect ShareID or RoomKey.'));
-                } else {
-                    alert(translate('Temporary server failure.'
-                                    + ' Please try again in a few minutes.'));
+        remote_login: function (login_info, url) {
+            $.ajax({
+                url: url || storage_root,
+                type: 'POST',
+                dataType: 'jsonp',
+                data: login_info,
+                success: function (data) {
+                    var match = data.match(/^(login|location):(.+)$/m);
+                    if (!match) {
+                        alert(translate('Temporary server failure. Please'
+                                        + ' try again in a few minutes.'));
+                    } else if (match[1] == 'login') {
+                        remote_login(login_info, match[2]);
+                    } else {
+                        window.location.href = match[2];
+                    }
+                },
+                error: function (xhr) {
+                    if (xhr.status == 403) {
+                        alert(translate('Incorrect username or password.'));
+                    } else if (xhr.status == 404) {
+                        alert(translate('Incorrect ShareID or RoomKey.'));
+                    } else {
+                        alert(translate('Temporary server failure. Please'
+                                        + ' try again in a few minutes.'));
+                    }
                 }
-            }
-        });
+            });
+        }
     }
-};
+}();
