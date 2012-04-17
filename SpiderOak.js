829ff7e62c655fdc8a5f75d43a2afd1b08a0e15e
Ken Manheimer <ken.manheimer@gmail.com>
2012-04-16 20:03:33 -0400

    Login seems to be working. Workaround in lieu of CORS and JSONP access is to start my browser (chrome) with options to inhibit web security. Sigh.
            --allow-file-access-from-files --allow-http-access-from-files
            --disable-web-security --enable-file-cookies
    
    Starting to craft visit_storage_node() routine that will get and handle the
    details of a storage node.
    
    Still need to confirm that the relaying-login case.

--- SpiderOak.js
+++ SpiderOak.js
@@ -21,7 +21,12 @@ $(document).ready(function() {
 var spideroak = function() {
     /* private: */
     /* "?callback=" is automatically included if $.ajax(dataType: 'jsonp') */
-    var storage_root = "https://spideroak.com/storage/%s/login";
+    var server_host_url = "https://spideroak.com";
+    var storage_login_path = "/storage/%s/login";
+    var storage_root_path;
+    /* XXX storage_node_page will likely branch to _root_ / _folder_ versions */
+    var storage_root_page = "storage-root";
+    var storage_folder_page = "storage-folder";
 
     /* public: */
     return {
@@ -29,7 +34,7 @@ var spideroak = function() {
             /* Nothing, yet. */
             },
         remote_login: function (login_info, url) {
-            var url = url || storage_root
+            var url = url || (server_host_url + storage_login_path)
             var login_url = url.replace(/%s/,
                                         b32encode_trim(login_info['username']));
             $.ajax({
@@ -37,16 +42,20 @@ var spideroak = function() {
                 type: 'POST',
                 dataType: 'text',
                 data: login_info,
-                crossDomain: true,
                 success: function (data) {
                     var match = data.match(/^(login|location):(.+)$/m);
                     if (!match) {
                         alert(translate('Temporary server failure. Please'
                                         + ' try again in a few minutes.'));
                     } else if (match[1] == 'login') {
+                        /* XXX Must confirm this case */
+                        // Relay to other server.
                         remote_login(login_info, match[2]);
                     } else {
-                        window.location.href = match[2];
+                        // Browser haz auth cookies, we haz relative location.
+                        storage_root_path = match[2];
+                        spideroak.visit_storage_node(server_host_url,
+                                                storage_root_path);
                     }
                 },
                 error: function (xhr) {
@@ -54,6 +63,31 @@ var spideroak = function() {
                         alert(translate('Incorrect username or password.'));
                     } else if (xhr.status == 404) {
                         alert(translate('Incorrect ShareID or RoomKey.'));
+                    } else {
+                        alert(translate('Temporary server failure. Please'
+                                        + ' try again in a few minutes.'));
+                    }
+                }
+            });
+        },
+        visit_storage_node: function (storage_host_url, storage_path) {
+            alert("visit_storage_node:\n   host: "
+                  + storage_host_url + "\n   path: " + storage_path);
+            $.ajax({
+                url: storage_host_url + storage_path,
+                type: 'POST',
+                dataType: 'text',
+                success: function (data) {
+                    alert(storage_host_url + storage_path + " data:\n"
+                          + data);
+                    },
+                error: function (xhr) {
+                    if (xhr.status == 403) {
+                        /* XXX Elaborate. */
+                        alert(translate('403'));
+                    } else if (xhr.status == 404) {
+                        /* XXX Elaborate. */
+                        alert(translate('404'));
                     } else {
                         alert(translate('Temporary server failure. Please'
                                         + ' try again in a few minutes.'));
