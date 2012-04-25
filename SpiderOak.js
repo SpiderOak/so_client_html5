b54d3bceb24565831a9ec7ec057302329759042b
Ken Manheimer <ken.manheimer@gmail.com>
2012-04-25 14:26:11 -0400

    Get Login and storage JSON retrieval working for both main server and dc2 accounts.
    - Organize the instance variables to distinguish default locations and
      those specific to the account ("my.").
    - For clarity, explicitly pass the default_locations.storage_path to
      .set_storage_parameters(), though it's (currently) always the same.
    - Make the error reporting thang take status code, rather than entire
      xhr. And use the routine for the top "Temporary server failure."
    - Use a 'blather()' routine from misc for debugging messages.

--- SpiderOak.js
+++ SpiderOak.js
@@ -21,21 +21,23 @@ $(document).ready(function() {
 var spideroak = function() {
     /* private: */
     // XXX server_host_url may vary, eg according to brand/package criteria.
-    var default_server_host_url = "https://spideroak.com";
-    var server_host_url;
-    var storage_login_path = "/browse/login";
-    var storage_path;
-    // API v1 (per https://spideroak.com/apis/partners/web_storage_api)
-    var storage_root = "https://spideroak.com/storage/";
-    var share_root = "https://spideroak.com/share/";
-    // Proposed API v2 (per https://spideroak.com/pandora/wiki/NewJsonObjectApi)
-    //var storage_root = "https://spideroak.com/webapi2/storage/";
-    //var share_root = "https://spideroak.com/webapi2/share/";
-    //var mobile_root = "https://spideroak.com/webapi2/mobile/";
-    var storage_root_user;
-    var share_root_user;
-    var storage_root_page_type = "storage-root";
-    var storage_folder_page_type = "storage-folder";
+    var default_locations = {
+        host_url: "https://spideroak.com",
+        storage_login_path: "/browse/login",
+        // API v1 per https://spideroak.com/apis/partners/web_storage_api:
+        //(Proposed API v2: https://spideroak.com/pandora/wiki/NewJsonObjectApi)
+        storage_url: "https://spideroak.com",
+        storage_path: "/storage/",
+        share_root: "https://spideroak.com/share/",
+        storage_page_div_type: "storage-root",
+        storage_folder_div_type: "storage-folder",
+    };
+    var my = {
+        host_url: null,
+        storage_path: null,
+        username: null,
+        storage_web_url: null,  // Hardly relevant - web UI for  user's storage.
+    };
     var device_info_query = '?device_info=yes';
 
     /* public: */
@@ -45,12 +47,14 @@ var spideroak = function() {
             },
         remote_login: function (login_info, url) {
             var login_url;
+            var server_host_url;
             if (url && (url.slice(0,4) === "http")) {
                 server_host_url = url.split('/').slice(0,3).join('/');
                 login_url = url
             } else {
-                server_host_url = default_server_host_url;
-                login_url = server_host_url + storage_login_path;
+                server_host_url = default_locations.host_url;
+                login_url = (server_host_url
+                             + default_locations.storage_login_path);
             }
             $.ajax({
                 url: login_url,
@@ -60,8 +64,8 @@ var spideroak = function() {
                 success: function (data) {
                     var match = data.match(/^(login|location):(.+)$/m);
                     if (!match) {
-                        alert(translate('Temporary server failure. Please'
-                                        + ' try again in a few minutes.'));
+                        error_alert('Temporary server failure. Please'
+                                    + ' try again in a few minutes.');
                     } else if (match[1] === 'login') {
                         if (match[2].charAt(0) === "/") {
                             login_url = server_host_url + match[2];
@@ -71,51 +75,65 @@ var spideroak = function() {
                         spideroak.remote_login(login_info, login_url);
                     } else {
                         // Browser haz auth cookies, we haz relative location.
-                        storage_path = match[2];
-                        spideroak.set_storage_user(login_info['username']);
+                        spideroak.set_storage_parameters(
+                            login_info['username'],
+                            server_host_url,
+                            default_locations.storage_path,
+                            match[2]);
                         spideroak.visit_storage_devices();
                     }
                 },
                 error: function (xhr) {
-                    error_alert("SpiderOak Login", xhr);
+                    error_alert("SpiderOak Login", xhr.status);
                 }
             });
         },
-        set_storage_user: function (username) {
-            /* Associate the username with the SpiderOak instance. */
-            storage_root_user = storage_root + b32encode_trim(username) + "/";
+        set_storage_parameters: function (username, host_url,
+                                          storage_path, storage_web_url) {
+            /* Register SpiderOak instance user-specific details. */
+            my.username = username;
+            my.host_url = host_url;
+            my.storage_path = storage_path;
+            my.storage_root_url = (my.host_url
+                                   + my.storage_path
+                                   + b32encode_trim(username));
+            my.storage_web_url = storage_web_url;
         },
         visit_storage_devices: function () {
             /* Visit the device directory, asking for detailed device info. */
-            spideroak.visit_storage_node("", device_info_query);
+            spideroak.visit_storage_node("/", device_info_query);
         },
-        visit_storage_node: function (storage_path, query) {
-            var storage_url = storage_root_user + storage_path
-            if (typeof query !== 'undefined') { storage_url += query; }
-            alert("visit_storage_node: " + storage_url + "\n");
+        visit_storage_node: function (storage_path, query_string) {
+            /* Focus on storage_path relative to user's storage root URL.
+               storage_path must be relative to root, and begin with '/'.
+               query_string, if present, is appended, for e.g. device_info. */
+            var storage_url = my.storage_root_url + storage_path;
+            if (typeof query_string !== 'undefined') {
+                storage_url += query_string; }
+            blather("visit_storage_node: " + storage_url + "\n");
             $.ajax({
                 url: storage_url,
                 type: 'GET',
                 dataType: 'json',
                 success: function (data) {
-                    alert("visit_storage_node " + storage_url + " got:\n"
+                    blather("visit_storage_node " + storage_url + " got:\n"
                           + JSON.stringify(data));
                     },
                 error: function (xhr) {
-                    error_alert("SpiderOak Storage Visit", xhr);
+                    error_alert("SpiderOak Storage Visit", xhr.status);
                 },
             });
         },
     }
 }();
 
-function error_alert(purpose, xhr) {
+function error_alert(purpose, status_code) {
     var msg = purpose + ": ";
-    if (xhr.status === 401) {
+    if (status_code === 401) {
         msg += 'Unauthorized.';
-    } else if (xhr.status === 403) {
+    } else if (status_code === 403) {
         msg += 'Incorrect username or password.';
-    } else if (xhr.status === 404) {
+    } else if (status_code === 404) {
         msg += 'Incorrect ShareID or RoomKey.';
     } else {
         msg += ('Temporary server failure. Please'
