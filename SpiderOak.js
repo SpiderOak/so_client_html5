7ddff58c54c5ee33414f2fcb6324e429f5fe1e35
Ken Manheimer <ken.manheimer@gmail.com>
2012-06-06 19:27:27 -0400

    Preserving this spiffy formulation of a settings manager before changing it to something a little less spiffy, but also a little less prone to mishap.
    This approach, using getters and setters, provides persistence for
    attribute assignments, as property or array elements, *once* you've
    registered the attributes. Problem is everything but persistence works if
    you fail to do the registration, leaving too much opportunity for
    hard-to-uncover bugs.
    
    I think we can't make this approach better because javascript doesn't seem
    to provide the means to trap arbitrary property assignments. So i'll be
    switching to more mundane .get()/ .set().

--- SpiderOak.js
+++ SpiderOak.js
@@ -734,7 +734,31 @@ var spideroak = function () {
         return "<" + this.emblem + ": " + this.url + ">";
     }
 
-    /* Content node collection management: */
+    /* Resource managers */
+
+    var SettingsManager = function () {
+        /* Maintain domain-specific persistent settings.
+           All values are returned as strings.
+           To establish a maintained setting, use .register(varname).
+           Settings assignment and retrieval work like object properties.
+         */
+        // A bit convoluted - we establish getters and setters for
+        // each of the keys already present in localStorage, to maintain
+        // internal consistency with what this.keys() reports.
+        var keys = this.keys();
+        for (key in keys) { this.register(keys[key]); }}
+
+    SettingsManager.prototype.register = function (name) {
+        this.__defineGetter__(name,
+                              function () {
+                                  return JSON.parse(localStorage[name]); });
+        this.__defineSetter__(name,
+                              function (value) { localStorage[name]
+                                                 = JSON.stringify(value); }); }
+    SettingsManager.prototype.keys = function () {
+        return Object.keys(localStorage); }
+    var sm = new SettingsManager();
+    sm.__defineGetter__('length', function () { return localStorage.length; });
 
     var content_node_manager = function () {
         /* A singleton utility for getting and removing content node objects.
