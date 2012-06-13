27df842b677183d1ebf5cce7b26e32ed5bc0b6b9
Ken Manheimer <ken.manheimer@gmail.com>
2012-06-13 17:00:01 -0400

    remember_manager.active(): properly qualify fields.

--- SpiderOak.js
+++ SpiderOak.js
@@ -1011,7 +1011,8 @@ var spideroak = function () {
             else if (typeof disposition === "undefined") {
                 return persistence_manager.get("remember_me") || false; }
             else {
-                for (var key in fields) { persistence_manager.remove(key); }
+                for (var key in persistence_manager.fields) {
+                    persistence_manager.remove(key); }
                 return persistence_manager.set("remember_me", false); }},
 
         fetch: function () {
