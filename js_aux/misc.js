1f39d7f0da866c5061d353ead2e08bd0713bafe5
Ken Manheimer <ken.manheimer@gmail.com>
2012-07-20 00:44:58 -0400

    Implement a provisional object-context .bind() poylfill.  Taken almost verbatim from developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind

--- js_aux/misc.js
+++ js_aux/misc.js
@@ -247,3 +247,28 @@ function deploy_focus_oneshot(selector, event) {
         $(selector).focus();
         $(document).unbind(event, focus_oneshot); }
     bind_replace($(document), event, focus_oneshot); }
+
+// Polyfill for context .bind(context), for it's lack in Safari.
+// Taken almost verbatim from
+// developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Function/bind
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function (oThis) {
+    if (typeof this !== "function") {
+      // closest thing possible to the ECMAScript 5 internal IsCallable function
+      throw new TypeError("Function.prototype.bind"
+                          + " - what is trying to be bound is not callable"); }
+
+    var aArgs = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP = function () {},
+        fBound = function () {
+          return fToBind.apply(this instanceof fNOP
+                               ? this
+                               : oThis,
+                               aArgs.concat(
+                                   Array.prototype.slice.call(arguments))); };
+      fNOP.prototype = this.prototype;
+      fBound.prototype = new fNOP();
+      return fBound;
+  };
+}
