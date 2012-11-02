bb323e37e67ba64d60f915f7364bb7433e337cff
Ken Manheimer <ken.manheimer@gmail.com>
2012-11-02 13:47:38 -0400

    Replace AVG brand artifacts with ones for fictitious "RR Acme" corporation.
    Crate logo adapted from image found, via google image search and
    intermediate web sites, at
    http://issresearchproject.grc.nasa.gov/Investigations/ACME/images/acmeLogo.gif

---
+++ release_artifacts/brand_settings_acme.js
@@ -0,0 +1,12 @@
+/* Settings specific to the branding of the application release. */
+var brand = {
+    title: "RR Acme",      // Full-blown application title.
+    label: "Acme",         // Concise, casual way to mention the app.
+    base_host_url: "https://acme.spideroak.com",
+    service_home_link: ('<a target="_blank" href="https://acme.spideroak.com"'
+                        + ' class="brand-service_home_link">'
+                        + 'RR Acme Service</a>'),
+    service_support_link: ('<a href="http://acme.spideroak.com/us-en/support"'
+                           + ' class="brand-service_support_link"'
+                           + ' target="_blank">RR Acme Support</a>'),
+}
