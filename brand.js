b355ace275883ba944f418abcf2f8fc1da4e89a6
Ken Manheimer <ken.manheimer@gmail.com>
2012-12-18 19:21:34 -0500

    Whoops - include a copy of the selected prototype/dev artifact brand.js.

--- brand.js
+++ brand.js
@@ -1,1 +1,12 @@
-release_artifacts/brand_settings_spideroak.js
+/* Settings specific to the branding of the application release. */
+var brand = {
+    title: "SpiderOak",
+    label: "SpiderOak",
+    base_host_url: "https://spideroak.com",
+    service_home_link: ('<a target="_blank" href="https://spideroak.com"'
+                        + ' class="brand-service_home_link">'
+                        + 'SpiderOak.com</a>'),
+    service_support_link: ('<a href="mailto:mobile@spideroak.com"'
+                           + ' class="brand-service_support_link"'
+                           + ' target="_blank">mobile@spideroak.com</a>'),
+}
