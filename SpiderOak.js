b74a34c8eb3d6e8b22468e0f1126685b91b65125
Ken Manheimer <ken.manheimer@gmail.com>
2012-12-29 00:49:50 -0500

    Implement my_icon_path for PanelNodes.

--- SpiderOak.js
+++ SpiderOak.js
@@ -2195,6 +2195,9 @@ var spideroak = function () {
     RootContentNode.prototype.my_icon_path = function () {
         return generic.brand_images_dir + "/brand_logo.png"; }
 
+    PanelNode.prototype.my_icon_path = function () {
+        return generic.brand_images_dir + "/settings.png"; }
+
     Node.prototype.here = function () {
         /* Return the complete address of this content node, as part of the
            application code, not just its JSON url.  */
