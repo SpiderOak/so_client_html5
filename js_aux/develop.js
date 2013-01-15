94b365e5b0d6e0fd62b5242f40549c220883e948
Ken Manheimer <ken.manheimer@gmail.com>
2013-01-15 17:01:34 -0500

    Provide an invaluable javascript read/eval/print loop, for poking and prodding in the device context.
    It runs under a prompt that pops up when the user enters "REPLPLEASE" on
    the device.  It is only present when SO_DEBUGGING includes ":repl:".

--- js_aux/develop.js
+++ js_aux/develop.js
@@ -24,6 +24,56 @@
  *
  * Include any non-empty string for general debugging messages.
  * ":verbose:" - additonal, UI-visible messages status messages
+ * ":repl:" - pop up a read/eval/print loop, when repl activation entered (see
+ *            "repl_key", below)
+ * ":content_urls:" - log content urls as they're traversed
  * ":test_keychain_trivial:" - elementary keychain plugin test
  */
-SO_DEBUGGING = ":basic: :content_urls: -test_keychain_trivial -verbose";
+SO_DEBUGGING = ":basic: :content_urls: :repl: -test_keychain_trivial- -verbose-";
+
+if (SO_DEBUGGING.match(/:repl:/)) {
+    /** Simple javascript read/eval/print loop, for poking and prodding.
+     *
+     * Runs in a javascript prompt().
+     */
+    function repl() {
+        var repl_expr = "", repl_result = "", repl_error;
+        while (repl_expr = prompt(repl_result + "" || "", "")) {
+            try {
+                repl_result = eval(repl_expr);
+            } catch(e) {
+                repl_result = "! " + e;
+            }
+        }
+    }
+
+    /** Launch repl when cue is noticed
+     *
+     * See cue variable for the cue phrase.
+     *
+     * We retain only the last character of input.
+     */
+    replSentinel = function () {
+        // Only upcase alphabetics - they map to lower case on the device!
+        var cue = "REPLPLEASE",
+            cue_at = 0;
+        return {
+            poll: function(repl_poll_event) {
+                var currkey = repl_poll_event.keyCode;
+                if (cue.charCodeAt(cue_at) === currkey) {
+                    cue_at += 1;
+                    if (cue.length === cue_at) {
+                        //console.log("repl cue satisfied");
+                        repl();
+                    } else {
+                        //console.log("repl sentinel increment to: " + cue_at);
+                        ;
+                    }
+                } else {
+                    cue_at = 0;
+                }
+            },
+        }
+    }()
+    $(document).bind("keydown", replSentinel.poll);
+}
